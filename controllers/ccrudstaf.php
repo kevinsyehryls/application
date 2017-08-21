cbc46a13e2b48b1e316220c85d2eb7113031509f
kevinsyehryls <kevin.syehryls@gmail.com>
2017-08-21 19:25:59 +0800

    validasi pada fungsi tambah

--- controllers/ccrudstaf.php
+++ controllers/ccrudstaf.php
@@ -56,46 +56,65 @@ class Ccrudstaf extends CI_Controller {
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">×</span></button>
             <h4 class="modal-title">TAMBAH STAFF</h4>
-          </div>
+    </div>
+    <?php
+    $frmattributes = array(
+       "id" => "id_FrmAddStaf",
+       "name" => "FrmAddStaf"
+       );
+      echo form_open('cpage/halstaf',$frmattributes);
+    ?>
          <div class="modal-body">
         
       	  <div class="box-body">
             <div class="form-group">
               <label for="nik">NIK</label>
-              <input type="text" class="form-control" id="id_stafnik" placeholder="Ketik NIK">
+              <input type="text" class="form-control" id="id_stafnik" name="id_stafnik" placeholder="Ketik NIK" required>
+              <label for="id_stafnik" class="error"></label>
             </div>
             <div class="form-group">
               <label for="nama">Nama</label>
-              <input type="text" class="form-control" id="id_stafnama" placeholder="Ketik Nama">
+              <input type="text" class="form-control" id="id_stafnama" name="id_stafnama" placeholder="Ketik Nama" equired>
+              <label for="id_stafnama" class="error"></label>
             </div>
             <div class="form-group">
               <label for="jabatan">Jabatan</label>
-              <input type="text" class="form-control" id="id_stafjbt" placeholder="Ketik Jabatan">
+              <input type="text" class="form-control" id="id_stafjbt" name="id_stafjbt" placeholder="Ketik Jabatan" equired>
+              <label for="id_stafjbt" class="error"></label>
             </div>
             <div class="form-group">
               <label for="nohp">Nomor Handphone</label>
-              <input type="text" class="form-control" id="id_stafhp" placeholder="Ketik Nomor HP">
-            </div>
+              <input type="number" class="form-control" id="id_stafhp" name="id_stafhp" placeholder="Ketik Nomor HP" equired>
+              <label for="id_stafhp" class="error"></label>
             <div class="form-group">
               <label for="email">Email</label>
-              <input type="text" class="form-control" id="id_stafemail" placeholder="Ketik Email">
+              <input type="text" class="form-control" id="id_stafemail" name="id_stafemail" placeholder="Ketik Email" equired>
+              <label for="id_stafemail" class="error"></label>
             </div>
             <div class="form-group">
               <label for="notlp">Nomor Kantor</label>
-              <input type="text" class="form-control" id="id_stafnotlp" placeholder="Ketik Nomor Tlp Kantor">
+              <input type="number" class="form-control" id="id_stafnotlp" name="id_stafnotlp" placeholder="Ketik Nomor Tlp Kantor" equired>
+              <label for="id_stafnotlp" class="error"></label>
             </div>
              <div class="form-group">
               <label for="alamat">Alamat</label>
-              <textarea class="form-control" rows="3" id="id_stafalmt" placeholder="Ketik Alamat"></textarea>
+              <textarea class="form-control" rows="3" id="id_stafalmt" name="id_stafalmt" placeholder="Ketik Alamat" equired></textarea>
+              <label for="id_stafalmt" class="error"></label>
             </div>       
           </div>          
 		</div>
-          <div class="modal-footer">
-           <button id="id_stafbtn" type="button" class="btn btn-primary">Save</button>
-          </div>
-		<?php
-	}
-	
+    <div class="modal-footer">
+     <button id="id_stafbtn" type="button" class="btn btn-primary">Save</button>
+    </div>
+      <style>
+          .error{
+          color: red;
+          font-style: italic;
+          }
+      </style>
+      <?php
+      echo form_close();
+  }
 	public function showeditstaf(){
 		$this->load->model('mcrudstaf');
 		$query=$this->mcrudstaf->select1staf();
@@ -107,6 +126,9 @@ class Ccrudstaf extends CI_Controller {
               <span aria-hidden="true">×</span></button>
             <h4 class="modal-title">EDIT STAFF</h4>
           </div>
+
+
+          
          <div class="modal-body">
         
       	  <div class="box-body">
