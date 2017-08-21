221f6e84d37df38c3733016bac72f6cc8fc5aa7e
kevinsyehryls <kevin.syehryls@gmail.com>
2017-08-21 20:24:31 +0800

    perbaiki error user

--- controllers/ccrudusr.php
+++ controllers/ccrudusr.php
@@ -2,14 +2,14 @@
 
 class Ccrudusr extends CI_Controller {
 
-	/* i. function construct */
-	function __construct(){
-		parent::__construct();
-	}
-	
-	public function showusr(){
-		?>
-		<table id="example1" class="table table-bordered table-striped">
+  /* i. function construct */
+  function __construct(){
+    parent::__construct();
+  }
+  
+  public function showusr(){
+    ?>
+    <table id="example1" class="table table-bordered table-striped">
             <thead>
             <tr>
               <th width="3%">No</th>
@@ -22,12 +22,12 @@ class Ccrudusr extends CI_Controller {
             </thead>
            <tbody>            
             <?php
-			  $this->load->model('mcrudusr');
-       		  $query = $this->mcrudusr->selectusr();
-			  $i = 1;
-			  foreach($query->result() as $row){
-			  	?>
-				<tr>
+        $this->load->model('mcrudusr');
+            $query = $this->mcrudusr->selectusr();
+        $i = 1;
+        foreach($query->result() as $row){
+          ?>
+        <tr>
                   <td><?php echo $i ?></td>
                   <td><?php echo $row->id_user ?></td>
                   <td><?php echo $row->email ?></td>
@@ -36,25 +36,24 @@ class Ccrudusr extends CI_Controller {
                   <td>
                     <button onclick="EditUsr(<?=$row->id_user?>)" type="button" class="btn btn-primary btn-xs">Edit</button>
                     <button onclick="DelUsr(<?=$row->id_user?>)" type="button" class="btn btn-primary btn-xs">Delete</button>
-                  </td> 			                  
+                  </td>                         
                 </tr>
-				<?php
-				$i++;
-			  } 
-			  ?>               
+        <?php
+        $i++;
+        } 
+        ?>               
         </table>
-		<?php
-	}
-	
-	public function addusr(){
-		?>
-		<div class="modal-header">
+    <?php
+  }
+  
+  public function addusr(){
+    ?>
+    <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span></button>
             <h4 class="modal-title">TAMBAH USER</h4>
-		</div>
+    </div>
         <?php
-<<<<<<< HEAD
          $frmattributes = array(
              "id" => "id_FrmAddUsr",
              "name" => "FrmAddUsr"
@@ -62,28 +61,14 @@ class Ccrudusr extends CI_Controller {
          echo form_open('cpage/haluser',$frmattributes);
          ?>
         
-		<div class="modal-body">
+    <div class="modal-body">
         
            <div class="box-body">
-=======
-        $frmattributes = array(
-            "id" => "id_FrmAddUsr",
-            "name" => "FrmAddUsr"
-        );
-        echo form_open('cpage/haluser',$frmattributes);
-        ?>
-		<div class="modal-body">
-            <div class="box-body">
->>>>>>> b3790670dddd84c8c96d86564a4b13945793b0d8
                 <div class="form-group">
                     <label for="nik">NIK</label>
                     <input type="text" class="form-control" id="id_usrnik" name="id_usrnik" placeholder="Ketik NIK" required>
                     <label for="id_usrnik" class="error"></label>
-<<<<<<< HEAD
                </div>
-=======
-                </div>
->>>>>>> b3790670dddd84c8c96d86564a4b13945793b0d8
                 <div class="form-group">
                     <label for="email">Email address</label>
                     <input type="email" class="form-control" id="id_usremail" name="id_usremail" placeholder="Enter email" required>
@@ -93,11 +78,7 @@ class Ccrudusr extends CI_Controller {
                     <label for="Password">Password</label>
                     <input type="password" class="form-control" id="id_usrpass" name="id_usrpass" placeholder="Password" required>
                     <label for="id_usrpass" class="error"></label>
-<<<<<<< HEAD
                </div>
-=======
-                </div>
->>>>>>> b3790670dddd84c8c96d86564a4b13945793b0d8
                 <div class="form-group">
                     <label for="nama">Nama</label>
                     <input type="text" class="form-control" id="id_usrnama" name="id_usrnama" placeholder="Ketik Nama" required>
@@ -105,57 +86,34 @@ class Ccrudusr extends CI_Controller {
                 </div>
                 <div class="form-group">
                     <label for="level">Level</label>
-<<<<<<< HEAD
-                   	<select id="id_usrlevel" name="id_usrlevel" class="form-control" required>
+                    <select id="id_usrlevel" name="id_usrlevel" class="form-control" required>
                        <option value="">---- PILIH LEVEL ----</option>
                        <option value="Administrasi">Administrasi</option>
                        <option value="SPV">SPV</option>
-                   	</select>
+                    </select>
                     <label for="id_usrlevel" class="error"></label>
                 </div>   
           </div>          
-		</div>
+    </div>
         
-		<div class="modal-footer">
-			<button id="id_usrbtn" type="button" class="btn btn-primary">Save</button>
-		</div>
+    <div class="modal-footer">
+      <button id="id_usrbtn" type="button" class="btn btn-primary">Save</button>
+    </div>
         <style>
              .error{
                  color: red;
                  font-style: italic;
              }
          </style>
-			<?php
-			echo form_close();
-=======
-                    <select id="id_usrlevel" name="id_usrlevel" class="form-control" required>
-                        <option value="">---- PILIH LEVEL ----</option>
-                        <option value="Administrasi">Administrasi</option>
-                        <option value="SPV">SPV</option>
-                    </select>
-                    <label for="id_usrlevel" class="error"></label>
-                </div>
-            </div>
-        </div>
-        <div class="modal-footer">
-            <button id="id_usrbtn" type="button" class="btn btn-primary">Save</button>
-        </div>
-        <style>
-            .error{
-                color: red;
-                font-style: italic;
-            }
-        </style>
-		<?php
-        echo form_close();
->>>>>>> b3790670dddd84c8c96d86564a4b13945793b0d8
-	}
-	
-	public function showeditusr(){
-		$this->load->model('mcrudusr');
-		$query=$this->mcrudusr->select1user();
-		foreach($query->result() as $row){
-			?>
+      <?php
+      echo form_close();
+  }
+  
+  public function showeditusr(){
+    $this->load->model('mcrudusr');
+    $query=$this->mcrudusr->select1user();
+    foreach($query->result() as $row){
+      ?>
             <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span></button>
@@ -197,24 +155,24 @@ class Ccrudusr extends CI_Controller {
                 <button id="id_usrbtn" type="button" class="btn btn-primary" onclick="UpdUser()">Save changes</button>
             </div>
                 
-			<?php
-		}
-	}
-	
-	public function editusr(){
-		$this->load->model('mcrudusr');
-		$query = $this->mcrudusr->updateusr();	
-	}
-	
-	public function saveusr(){
-		$this->load->model('mcrudusr');
-		$query = $this->mcrudusr->insertusr();
-	}
-	
-	public function delusr(){
-		$this->load->model('mcrudusr');
-		$query = $this->mcrudusr->deleteusr();
-	}
+      <?php
+    }
+  }
+  
+  public function editusr(){
+    $this->load->model('mcrudusr');
+    $query = $this->mcrudusr->updateusr();  
+  }
+  
+  public function saveusr(){
+    $this->load->model('mcrudusr');
+    $query = $this->mcrudusr->insertusr();
+  }
+  
+  public function delusr(){
+    $this->load->model('mcrudusr');
+    $query = $this->mcrudusr->deleteusr();
+  }
 
 }
 ?>
