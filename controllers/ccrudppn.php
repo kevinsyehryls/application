8823218f0e9161a5fc1b358034568a8b4c13bb75
kevinsyehryls <kevin.syehryls@gmail.com>
2017-08-21 19:25:47 +0800

    validasi pada fungsi tambah

--- controllers/ccrudppn.php
+++ controllers/ccrudppn.php
@@ -50,28 +50,46 @@ class Ccrudppn extends CI_Controller {
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">×</span></button>
             <h4 class="modal-title">TAMBAH PIMPINAN TELKOMSEL</h4>
-          </div>
-         <div class="modal-body">
-        
+    </div>
+        <?php
+         $frmattributes = array(
+             "id" => "id_FrmAddPpn",
+             "name" => "FrmAddPpn"
+         );
+         echo form_open('cpage/halppn',$frmattributes);
+        ?>
+    <div class="modal-body">
+
       	  <div class="box-body">
             <div class="form-group">
               <label for="nik">NIK</label>
-              <input type="text" class="form-control" id="id_ppnnik" placeholder="Ketik NIK">
+              <input type="text" class="form-control" id="id_ppnnik" name="id_ppnnik" placeholder="Ketik NIK" required>
+                   <label for="id_ppnnik" class="error"></label>
             </div>
             <div class="form-group">
               <label for="nama">Nama</label>
-              <input type="text" class="form-control" id="id_ppnnama" placeholder="Ketik Nama">
+              <input type="text" class="form-control" id="id_ppnnama" name="id_ppnnama" placeholder="Ketik Nama" required>
+                   <label for="id_ppnnama" class="error"></label>
             </div>
             <div class="form-group">
               <label for="jabatan">Jabatan</label>
-              <input type="text" class="form-control" id="id_ppnjbt" placeholder="Ketik Jabatan">
+              <input type="text" class="form-control" id="id_ppnjbt" name="id_ppnjbt" placeholder="Ketik Jabatan" required>
+                   <label for="id_ppnjbt" class="error"></label>
             </div>      
-          </div>          
+          </div>
+
 		</div>
           <div class="modal-footer">
            <button id="id_ppnbtn" type="button" class="btn btn-primary">Save</button>
-          </div>
-		<?php
+          </div> 
+    <style>
+      .error{
+      color: red;
+      font-style: italic;
+             }
+    </style>
+      <?php
+      echo form_close();
 	}
 	
 	public function showeditppn(){
