f8339cda58b5ea3a49f672eb42acb08485b7c501
kevinsyehryls <kevin.syehryls@gmail.com>
2017-08-21 19:28:56 +0800

    validasi pada fungsi tambah

--- views/pages/vppn.php
+++ views/pages/vppn.php
@@ -48,25 +48,44 @@
 	// ketika DOM ready
 	$(document).ready(function(){
 		GenDataPpn();
-	})
+	});
 	
 	// ketika tombol tambah user di klik
 	$(document).on('click', '#id_BtnAddPpn', function(){
 		// tampilkan modal
 		$('#modal-default').modal('show');
-		// isi modal dengan form add user
+		// isi modal dengan form add ppn
 		jQuery.ajax({
             type: "POST",
             url: "<?php echo base_url(); ?>" + "index.php/ccrudppn/addppn",
             success: function(res) {
-                $('#id_MdlDefault').html(res);
-				SavePpn();
-            },
-            error: function(xhr){
-               $('#id_MdlDefault').html("error");
-            }
-        });		
-	})
+                $('#id_MdlDefault').html(res); 
+                // form validation on ready state
+                 $().ready(function(){
+                     $('#id_FrmAddPpn').validate({
+                         rules:{
+                             id_ppnnik: {
+                                required: true,
+                                maxlength: 5
+                             },
+                             id_ppnnama: "required",
+                             id_ppnjbt: "required",
+                         },
+                         messages: {
+                             id_ppnnik: "isi NIK dengan benar",
+                             id_ppnnama: "isi nama dengan benar",
+                             id_ppnjbt: "isi Jabatan dengan benar"
+                        }
+                     });
+                 });
+  				SavePpn();
+              },
+              error: function(xhr){
+                 $('#id_MdlDefault').html("error");
+              }
+          });		
+	 });
+
 	
 	// function untuk populate data user dari table database
 	function GenDataPpn(){
@@ -94,26 +113,33 @@
 	
 	// save user
 	function SavePpn(){
-		$(document).on('click', '#id_ppnbtn', function(){
-			jQuery.ajax({
-            type: "POST",
-            url: "<?php echo base_url(); ?>" + "index.php/ccrudppn/saveppn",
-			data: {
-				id_ppnnik: $('#id_ppnnik').val(),
-				id_ppnnama: $('#id_ppnnama').val(),
-				id_ppnjbt: $('#id_ppnjbt').val()
-			},
-            success: function(res) {
-				$('#modal-default').modal('hide');
-				alert("Data saved!");
-				GenDataPpn();
-			},
-            error: function(xhr){
-               $('#id_DivPpn').html("error");
-            }
-        });
-		})
-	}
+		$(document).on('click', '#id_ppnbtn', function(e){
+			e.preventDefault();
+            	if($('#id_FrmAddPpn').valid()){
+             		// jika validasi berhasil
+                 jQuery.ajax({
+                     type: "POST",
+                     url: "<?php echo base_url(); ?>" + "index.php/ccrudppn/saveppn",
+                     data: {
+                         id_ppnnik: $('#id_ppnnik').val(),
+                         id_ppnnama: $('#id_ppnnama').val(),
+                         id_ppnjbt: $('#id_ppnjbt').val()
+                     },
+                     success: function(res) {
+                        $('#modal-default').modal('hide');
+                        alert("Data saved!");
+                         GenDataPpn();
+                     },
+                    error: function(xhr){
+                         $('#id_DivPpn').html("error");
+                     }
+                 });
+             	} else {
+            	// dan jika gagal
+                 return false;
+              	}
+  		})
+  	}
 	
 	//Saat Tombol Edit di Klik
 	function EditPpn(id_pimpinan_telkomsel){
