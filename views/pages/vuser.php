1191a96c2fdf27e0598afbdc26f707adff2dbf9a
kevinsyehryls <kevin.syehryls@gmail.com>
2017-08-21 19:29:24 +0800

    validasi pada fungsi tambah

--- views/pages/vuser.php
+++ views/pages/vuser.php
@@ -40,36 +40,63 @@
         <!-- /.box-body -->
           </div>
       <!-- /.box -->
-
     </section>
     <!-- /.content -->
   </div>
-  
-  
+
 <script>
 	// ketika DOM ready
 	$(document).ready(function(){
 		GenDataUsr();
-	})
+	});
 	
 	// ketika tombol tambah user di klik
 	$(document).on('click', '#id_BtnAddUsr', function(){
-		// tampilkan modal
-		$('#modal-default').modal('show');
-		// isi modal dengan form add user
-		jQuery.ajax({
-            type: "POST",
-            url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/addusr",
-            success: function(res) {
-                $('#id_MdlDefault').html(res);
-				SaveUsr();
-            },
-            error: function(xhr){
-               $('#id_MdlDefault').html("error");
-            }
-        });		
-	})
-	
+ 		// tampilkan modal
+ 		$('#modal-default').modal('show');
+ 		// isi modal dengan form add user
+ 		jQuery.ajax({
+             type: "POST",
+              url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/addusr",
+              success: function(res) {
+                  $('#id_MdlDefault').html(res);
+                 // form validation on ready state
+                 $().ready(function(){
+                     $('#id_FrmAddUsr').validate({
+                         rules:{
+                             id_usrnik: {
+                                 required: true,
+                                 maxlength: 6,
+                                 minlength: 5
+                             },
+                             id_usremail: {
+                                 required: true,
+                                 email: true
+                             },
+                             id_usrpass: {
+                                 required: true,
+                                 maxlength: 9,
+                                 minlength: 4
+                             },
+                             id_usrnama: "required",
+                             id_usrlevel: "required"
+                         },
+                         messages: {
+                             id_usrnik: "isi id dengan benar",
+                             id_usremail: "isi email dengan benar",
+                             id_usrpass: "isi password dengan benar",
+                             id_usrnama: "isi nama dengan benar",
+                             id_usrlevel: "isi level dengan benar"
+                        }
+                     });
+                 });
+  				SaveUsr();
+              },
+              error: function(xhr){
+                 $('#id_MdlDefault').html("error");
+              }
+          });		
+	 });
 
 	// function untuk populate data user dari table database
 	function GenDataUsr(){
@@ -97,28 +124,35 @@
 	
 	// save user
 	function SaveUsr(){
-		$(document).on('click', '#id_usrbtn', function(){
-			jQuery.ajax({
-            type: "POST",
-            url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/saveusr",
-			data: {
-				id_usrnik: $('#id_usrnik').val(),
-				id_usremail: $('#id_usremail').val(),
-				id_usrpass: $('#id_usrpass').val(),
-				id_usrnama: $('#id_usrnama').val(),
-				id_usrlevel: $('#id_usrlevel').val()
-			},
-            success: function(res) {
-				$('#modal-default').modal('hide');
-				alert("Data saved!");
-				GenDataUsr();
-			},
-            error: function(xhr){
-               $('#id_DivUsr').html("error");
-            }
-        });
-		})
-	}
+		$(document).on('click', '#id_usrbtn', function(e){
+		    e.preventDefault();
+            if($('#id_FrmAddUsr').valid()){
+             // jika validasi berhasil
+                 jQuery.ajax({
+                     type: "POST",
+                     url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/saveusr",
+                     data: {
+                         id_usrnik: $('#id_usrnik').val(),
+                         id_usremail: $('#id_usremail').val(),
+                         id_usrpass: $('#id_usrpass').val(),
+                       	 id_usrnama: $('#id_usrnama').val(),
+                         id_usrlevel: $('#id_usrlevel').val()
+                     },
+                     success: function(res) {
+                        $('#modal-default').modal('hide');
+                        alert("Data saved!");
+                         GenDataUsr();
+                     },
+                    error: function(xhr){
+                         $('#id_DivUsr').html("error");
+                     }
+                 });
+             } else {
+            // dan jika gagal
+                 return false;
+              }
+  		})
+  	}
 	
 	//Saat Tombol Edit di Klik
 	function EditUsr(id_user){
