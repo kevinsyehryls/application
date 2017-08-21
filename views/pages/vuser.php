1eaa63d7d07ba5f9b59f9f61540c3e0e4ff1e856
kevinsyehryls <kevin.syehryls@gmail.com>
2017-08-21 20:25:02 +0800

    perbaiki error user

--- views/pages/vuser.php
+++ views/pages/vuser.php
@@ -3,7 +3,7 @@
     <div class="modal fade" id="modal-default" style="display: none;">
       <div class="modal-dialog">
         <div id="id_MdlDefault" class="modal-content">
-        	<!-- isi modal dinamis disini -->
+          <!-- isi modal dinamis disini -->
         </div>
         <!-- /.modal-content -->
       </div>
@@ -30,12 +30,12 @@
       <div class="box">
         <div class="box-header with-border">
           <h3 class="box-title">Kelola User</h3>
-              <button type="button" id="id_BtnAddUsr" class="btn btn-primary btn-sm pull-right">Tambah User	</button>
+              <button type="button" id="id_BtnAddUsr" class="btn btn-primary btn-sm pull-right">Tambah User </button>
         </div>
         <div class="box-body">
-        	<div id="id_DivUsr">
-            	<!-- data user akan tampil disini -->
-            </div>	
+          <div id="id_DivUsr">
+              <!-- data user akan tampil disini -->
+            </div>  
         </div>
         <!-- /.box-body -->
           </div>
@@ -43,26 +43,19 @@
     </section>
     <!-- /.content -->
   </div>
-<<<<<<< HEAD
 
-=======
-  
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
->>>>>>> b3790670dddd84c8c96d86564a4b13945793b0d8
 <script>
-	// ketika DOM ready
-	$(document).ready(function(){
-		GenDataUsr();
-	});
-	
-	// ketika tombol tambah user di klik
-	$(document).on('click', '#id_BtnAddUsr', function(){
-<<<<<<< HEAD
- 		// tampilkan modal
- 		$('#modal-default').modal('show');
- 		// isi modal dengan form add user
- 		jQuery.ajax({
+  // ketika DOM ready
+  $(document).ready(function(){
+    GenDataUsr();
+  });
+  
+  // ketika tombol tambah user di klik
+  $(document).on('click', '#id_BtnAddUsr', function(){
+    // tampilkan modal
+    $('#modal-default').modal('show');
+    // isi modal dengan form add user
+    jQuery.ajax({
              type: "POST",
               url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/addusr",
               success: function(res) {
@@ -97,84 +90,43 @@
                         }
                      });
                  });
-  				SaveUsr();
+          SaveUsr();
               },
               error: function(xhr){
                  $('#id_MdlDefault').html("error");
               }
-          });		
-	 });
-=======
-		// tampilkan modal
-		$('#modal-default').modal('show');
-		// isi modal dengan form add user
-		jQuery.ajax({
-            type: "POST",
-            url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/addusr",
-            success: function(res) {
-                $('#id_MdlDefault').html(res);
-                // form validation on ready state
-                $().ready(function(){
-                    $('#id_FrmAddUsr').validate({
-                        rules:{
-                            id_usrnik: "required",
-                            id_usremail: {
-                                required: true,
-                                email: true
-                            },
-                            id_usrpass: "required",
-                            id_usrnama: "required",
-                            id_usrlevel: "required"
-                        },
-                        messages: {
-                            id_usrnik: "id required",
-                            id_usremail: "email required",
-                            id_usrpass: "pass required",
-                            id_usrnama: "nama required",
-                            id_usrlevel: "level required"
-                        }
-                    });
-                });
-				SaveUsr();
-            },
-            error: function(xhr){
-               $('#id_MdlDefault').html("error");
-            }
-        });		
-	});
-	
->>>>>>> b3790670dddd84c8c96d86564a4b13945793b0d8
+          });   
+   });
 
-	// function untuk populate data user dari table database
-	function GenDataUsr(){
-		jQuery.ajax({
+  // function untuk populate data user dari table database
+  function GenDataUsr(){
+    jQuery.ajax({
             type: "POST",
             url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/showusr",
             success: function(res) {
                 $('#id_DivUsr').html(res);
-				$(function() {
-					$('#example1').DataTable({
-						'paging'      : true,
-						'lengthChange': false,
-						'searching'   : true,
-						'ordering'    : true,
-						'info'        : true,
-						'autoWidth'   : true
-					})
-				})
+        $(function() {
+          $('#example1').DataTable({
+            'paging'      : true,
+            'lengthChange': false,
+            'searching'   : true,
+            'ordering'    : true,
+            'info'        : true,
+            'autoWidth'   : true
+          })
+        })
             },
             error: function(xhr){
                $('#id_DivUsr').html("error");
             }
         });
-	}
-	
-	// save user
-	function SaveUsr(){
-		$(document).on('click', '#id_usrbtn', function(e){
-		    e.preventDefault();
+  }
+  
+  // save user
+  function SaveUsr(){
+    $(document).on('click', '#id_usrbtn', function(e){
+        e.preventDefault();
             if($('#id_FrmAddUsr').valid()){
-<<<<<<< HEAD
              // jika validasi berhasil
                  jQuery.ajax({
                      type: "POST",
@@ -183,7 +135,7 @@
                          id_usrnik: $('#id_usrnik').val(),
                          id_usremail: $('#id_usremail').val(),
                          id_usrpass: $('#id_usrpass').val(),
-                       	 id_usrnama: $('#id_usrnama').val(),
+                         id_usrnama: $('#id_usrnama').val(),
                          id_usrlevel: $('#id_usrlevel').val()
                      },
                      success: function(res) {
@@ -199,97 +151,69 @@
             // dan jika gagal
                  return false;
               }
-  		})
-  	}
-=======
-            // jika validasi berhasil
-                jQuery.ajax({
-                    type: "POST",
-                    url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/saveusr",
-                    data: {
-                        id_usrnik: $('#id_usrnik').val(),
-                        id_usremail: $('#id_usremail').val(),
-                        id_usrpass: $('#id_usrpass').val(),
-                        id_usrnama: $('#id_usrnama').val(),
-                        id_usrlevel: $('#id_usrlevel').val()
-                    },
-                    success: function(res) {
-                        $('#modal-default').modal('hide');
-                        alert("Data saved!");
-                        GenDataUsr();
-                    },
-                    error: function(xhr){
-                        $('#id_DivUsr').html("error");
-                    }
-                });
-            } else {
-            // dan jika gagal
-                return false;
-            }
-		})
-	}
->>>>>>> b3790670dddd84c8c96d86564a4b13945793b0d8
-	
-	//Saat Tombol Edit di Klik
-	function EditUsr(id_user){
-		$('#modal-default').modal('show');
-		jQuery.ajax({
-				type: "POST",
-				url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/showeditusr",
-				data: {
-					id_usrnik: id_user
-				},
-				success: function(res) {
-					$('#id_MdlDefault').html(res);
-				},
-				error: function(xhr){
-				   $('#id_DivUsr').html("error");
-				}
-			});
-	}
-	
-	//Saat tombol save change di klik
-	function UpdUser(){
-		jQuery.ajax({
-			type: "POST",
-			url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/editusr",
-			data: {
-				id_usrnik: $('#id_usrnik').val(),
-				id_usremail: $('#id_usremail').val(),
-				id_usrpass: $('#id_usrpass').val(),
-				id_usrnama: $('#id_usrnama').val(),
-				id_usrlevel: $('#id_usrlevel').val()
-			},
-			success: function(res) {
-				$('#modal-default').modal('hide');
-				alert("Data Updated!");
-				GenDataUsr();
-			},
-			error: function(xhr){
-			   $('#id_DivUsr').html("error");
-			}
-		});
-	}
-	
-	function DelUsr(id_user){
-		var delconf = confirm("Hapus data?");
-		if(delconf){
-			jQuery.ajax({
-				type: "POST",
-				url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/delusr",
-				data: {
-					id_usrnik: id_user
-				},
-				success: function(res) {
-					$('#modal-default').modal('hide');
-					alert("Data Terhapus!");
-					GenDataUsr();
-				},
-				error: function(xhr){
-				   $('#id_DivUsr').html("error");
-				}
-			});
-		}		
-	}
-	
+      })
+    }
+  
+  //Saat Tombol Edit di Klik
+  function EditUsr(id_user){
+    $('#modal-default').modal('show');
+    jQuery.ajax({
+        type: "POST",
+        url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/showeditusr",
+        data: {
+          id_usrnik: id_user
+        },
+        success: function(res) {
+          $('#id_MdlDefault').html(res);
+        },
+        error: function(xhr){
+           $('#id_DivUsr').html("error");
+        }
+      });
+  }
+  
+  //Saat tombol save change di klik
+  function UpdUser(){
+    jQuery.ajax({
+      type: "POST",
+      url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/editusr",
+      data: {
+        id_usrnik: $('#id_usrnik').val(),
+        id_usremail: $('#id_usremail').val(),
+        id_usrpass: $('#id_usrpass').val(),
+        id_usrnama: $('#id_usrnama').val(),
+        id_usrlevel: $('#id_usrlevel').val()
+      },
+      success: function(res) {
+        $('#modal-default').modal('hide');
+        alert("Data Updated!");
+        GenDataUsr();
+      },
+      error: function(xhr){
+         $('#id_DivUsr').html("error");
+      }
+    });
+  }
+  
+  function DelUsr(id_user){
+    var delconf = confirm("Hapus data?");
+    if(delconf){
+      jQuery.ajax({
+        type: "POST",
+        url: "<?php echo base_url(); ?>" + "index.php/ccrudusr/delusr",
+        data: {
+          id_usrnik: id_user
+        },
+        success: function(res) {
+          $('#modal-default').modal('hide');
+          alert("Data Terhapus!");
+          GenDataUsr();
+        },
+        error: function(xhr){
+           $('#id_DivUsr').html("error");
+        }
+      });
+    }   
+  }
+  
 </script>
