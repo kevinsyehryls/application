aff74758d5aa4ffdd20edd7503f02aaaf184ed26
kevinsyehryls <kevin.syehryls@gmail.com>
2017-08-26 01:03:22 +0800

    dasboard

--- views/pages/vindx.php
+++ views/pages/vindx.php
@@ -9,83 +9,36 @@
 
     <!-- Main content -->
     <section class="content">
+         <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>150</h3>
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">PKS PENDING PROSES</h3>
-        </div>
-        <div class="box-body">
-          <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th width="3%">No</th>
-                  <th width="12%">NIK</th>
-                  <th width="25%">Email</th>
-                  <th width="25%">Nama</th>
-                  <th width="10%">Area</th>
-                  <th width="10%">Level</th>
-                  <th width="15%">Opsi</th>
-                </tr>
-                </thead>
-               <tbody>
-               <tr>
-                  <td>1</td>
-                  <td>1300001</td>
-                  <td>kevinsyehryls@gmail.com</td>
-                  <td>kevin</td>
-                  <td>AREA III</td>
-                  <td>Administrasi</td>
-                  <td>
-                  	<button type="button" class="btn btn-primary btn-xs">Tambah</button>
-                    <button type="button" class="btn btn-primary btn-xs">Edit</button>
-                  </td>
-                </tr>
-                
-              </table>	
-        </div>
-        <!-- /.box-body -->
+              <p>New Orders</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
-      <!-- /.box -->
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-<!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">PKS AKAN BERAKHIR </h3>
-        </div>
-        <div class="box-body">
-          <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th width="3%">No</th>
-                  <th width="12%">NIK</th>
-                  <th width="25%">Email</th>
-                  <th width="25%">Nama</th>
-                  <th width="10%">Area</th>
-                  <th width="10%">Level</th>
-                  <th width="15%">Opsi</th>
-                </tr>
-                </thead>
-               <tbody>
-               <tr>
-                  <td>1</td>
-                  <td>1300001</td>
-                  <td>kevinsyehryls@gmail.com</td>
-                  <td>kevin</td>
-                  <td>AREA III</td>
-                  <td>Administrasi</td>
-                  <td>
-                  	<button type="button" class="btn btn-primary btn-xs">Tambah</button>
-                    <button type="button" class="btn btn-primary btn-xs">Edit</button>
-                  </td>
-                </tr>
-                
-              </table>	
-        </div>
-        <!-- /.box-body -->
+              <p>Bounce Rate</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
-      <!-- /.box -->
-
 
     </section>
     <!-- /.content -->
