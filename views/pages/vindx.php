9f1efead711204cd750e25f8eaddf13cdb477853
kevinsyehryls <kevin.syehryls@gmail.com>
2017-08-31 15:37:16 +0800

    update design dan merapikan design

--- views/pages/vindx.php
+++ views/pages/vindx.php
@@ -9,7 +9,7 @@
 
     <!-- Main content -->
     <section class="content">
-         <!-- Small boxes (Stat box) -->
+
       <div class="row">
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
@@ -17,12 +17,13 @@
             <div class="inner">
               <h3>150</h3>
 
-              <p>New Orders</p>
+              <p>PENDING TTD PIC</p>
+              <p>CORPORATE</p>
             </div>
             <div class="icon">
-              <i class="ion ion-bag"></i>
+              <i class="ion ion-alert-circled"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="#" class="small-box-footer">Info Lebih Lanjut <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -30,16 +31,48 @@
           <!-- small box -->
           <div class="small-box bg-green">
             <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Bounce Rate</p>
+              <h3>53</h3>
+              <p>PENDING TTD</p>
+              <p>PIMPINAN CORPORATE</p>
             </div>
             <div class="icon">
-              <i class="ion ion-stats-bars"></i>
+              <i class="ion ion-alert-circled"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="#" class="small-box-footer">Info Lebih Lanjut <i class="fa fa-arrow-circle-right"></i></a>
           </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>44</h3>
 
+              <p>PENDING TTD</p>
+              <p>PIC TELKOMSEL</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-alert-circled"></i>
+            </div>
+             <a href="#" class="small-box-footer">Info Lebih Lanjut <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>PENDING TTD</p>
+              <p>PIMPINAN TELKOMSEL</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-alert-circled"></i>
+            </div>
+             <a href="#" class="small-box-footer">Info Lebih Lanjut <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
     </section>
     <!-- /.content -->
   </div>
