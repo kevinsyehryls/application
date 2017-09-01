2a3065ec4ad94314c1dc99d9d5b538f4226a0850
kevinsyehryls <kevin.syehryls@gmail.com>
2017-09-01 17:26:12 +0800

    update auto_increment

--- telkomsel.sql
+++ telkomsel.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2017 at 11:21 AM
+-- Generation Time: Sep 01, 2017 at 11:24 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.21
 
@@ -27,14 +27,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tb_corporate` (
-  `id_corporate` varchar(15) NOT NULL,
+  `id_corporate` int(15) NOT NULL,
   `nama_corporate` varchar(50) NOT NULL,
   `alamat_corporate` text NOT NULL,
   `nama_pimpinan_corporate` varchar(50) NOT NULL,
   `jabatan_pimpinan_corporate` varchar(50) NOT NULL,
   `nama_pic_corporate` varchar(50) NOT NULL,
   `jabatan_pic_corporate` varchar(50) NOT NULL,
-  `nomor_hp_pic_corporate` varchar(12) NOT NULL,
+  `nomor_hp_pic_corporate` varchar(13) NOT NULL,
   `email_pic_corporate` varchar(100) NOT NULL,
   `nomor_tlp_kantor` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -44,10 +44,11 @@ CREATE TABLE `tb_corporate` (
 --
 
 INSERT INTO `tb_corporate` (`id_corporate`, `nama_corporate`, `alamat_corporate`, `nama_pimpinan_corporate`, `jabatan_pimpinan_corporate`, `nama_pic_corporate`, `jabatan_pic_corporate`, `nomor_hp_pic_corporate`, `email_pic_corporate`, `nomor_tlp_kantor`) VALUES
-('1', 'PT. Mirage Bali Distribution', 'sdsadsad', 'Thusen', 'Owner ', 'Thusen', 'Owner', '0811386772', 'mirage.bali@gmail.com', '0'),
-('2', 'PT. Bpr Sri Artha Lestari', '', 'Luh Ketut Citarasmini', 'Kadiv HCM & Complience', 'Ribka Kristin', 'Staff HCM', '0823772382', 'ribka.kristin@bprlestari.com', '2147483647'),
-('3', 'Potato Head And Club', 'Jl. Peti Tenget 51B Seminyak, Kuta, Badung, Bali', 'Ario Bimo Wicaksono', 'Financial Controller', 'Ni Ketut Suarini', 'Manager GA', '2147483647', 'suryani@pttfamily.com', '2147483647'),
-('4', 'PT. Livit International Indonesia', 'Rukan No 14 Perum Bumi Santi, Banjar Sasih,Jl. Pratu Made Rembug Batu Bulan 80571, Gianyar, Bali', 'Nicholas John Martin', 'Operational Director', 'Ni Luh Putu Yunari', 'Finance & Operation Assistant', '2147483647', 'invoice@liv.it', '361292123');
+(101, 'PT. Mirage Bali Distribution', 'swssdwdwdwdwd', 'Thusen', 'Owner', 'Thusen', 'Owner', '811386772', 'mirage.bali@gmail.com', '121332'),
+(102, 'PT. Bpr Sri Artha Lestari', 'wdwddw', 'Luh Ketut Citarasmini', 'Kadiv HCM & Complience', 'Ribka Kristin', 'Staff HCM', '823772382', 'ribka.kristin@bprlestari.com', '2147483647'),
+(103, 'Potato Head And Club', 'Jl. Peti Tenget 51B Seminyak, Kuta, Badung, Bali', 'Ario Bimo Wicaksono', 'Financial Controller', 'Ni Ketut Suarini', 'Manager GA', '2147483647', 'suryani@pttfamily.com', '323234'),
+(104, 'PT. Livit International Indonesia', 'Rukan No 14 Perum Bumi Santi, Banjar Sasih,Jl. Pratu Made Rembug Batu Bulan 80571, Gianyar, Bali', 'Nicholas John Martin', 'Operational Director', 'Ni Luh Putu Yunari', 'Finance & Operation Assistant', '2147483647', 'invoice@liv.it', '361292123'),
+(105, 'coba', 'coba', 'coba', 'coba', 'coba', 'coba', '1212122', 'coba@coba.com', '12212');
 
 -- --------------------------------------------------------
 
@@ -56,8 +57,8 @@ INSERT INTO `tb_corporate` (`id_corporate`, `nama_corporate`, `alamat_corporate`
 --
 
 CREATE TABLE `tb_list_nomor` (
-  `id_list_msisdn` varchar(15) NOT NULL,
-  `id_corporate` varchar(15) NOT NULL,
+  `id_list_msisdn` int(15) NOT NULL,
+  `id_corporate` int(15) NOT NULL,
   `msisdn` int(12) NOT NULL,
   `user` varchar(30) NOT NULL,
   `division` varchar(30) NOT NULL,
@@ -70,7 +71,7 @@ CREATE TABLE `tb_list_nomor` (
 --
 
 INSERT INTO `tb_list_nomor` (`id_list_msisdn`, `id_corporate`, `msisdn`, `user`, `division`, `short_code`, `deskripsi`) VALUES
-('2333', '1', 1111111111, '1111111', '1111111', '11111111', '11111111');
+(10001, 101, 323232, 'fvf', 'fvf', 'vfv', 'vf');
 
 -- --------------------------------------------------------
 
@@ -115,7 +116,7 @@ CREATE TABLE `tb_pic_telkomsel` (
 --
 
 INSERT INTO `tb_pic_telkomsel` (`id_pic_telkomsel`, `nama_pic_telkomsel`, `jabatan_pic_telkomsel`, `nomor_hp_pic_telkomsel`, `email_pic_telkomsel`, `nomor_tlp_kantor`, `alamat_kantor`) VALUES
-('73245', 'Ida Bagus Wisnu Singarsa', 'Account Manager', 811391737, 'ida_bw_singarsa@telkomsel.co.id', 361244235, 'Jl. Raya Puputan No.33, Renon, Denpasar Tim., Kota Denpasar, Bali 80234'),
+('73245', 'Ida Bagus Wisnu Singarsa', 'Account Manager', 811391737, 'ida_bw_singarsa@telkomsel.co.id', 361244235, 'dsds'),
 ('87928', 'Ni Komang Meigawati', 'Account Manager', 811390008, 'nikomang_meigawati@telkomsel.co.id', 361244235, 'Jl. Raya Puputan No.33, Renon, Denpasar Tim., Kota Denpasar, Bali 80234'),
 ('88132', 'Pandu Adi Setiawan', 'Account Manager', 2147483647, 'pandu_a_setiawan@telkomsel.co.id', 361244235, 'Jl. Raya Puputan No.33, Renon, Denpasar Tim., Kota Denpasar, Bali 80234'),
 ('88145', 'Jonathan', 'Account Manager', 2147483647, 'jonathan@telkomsel.co.id', 361244235, 'Jl. Raya Puputan No.33, Renon, Denpasar Tim., Kota Denpasar, Bali 80234'),
@@ -148,18 +149,18 @@ INSERT INTO `tb_pimpinan_telkomsel` (`id_pimpinan_telkomsel`, `nama_pimpinan_tel
 --
 
 CREATE TABLE `tb_pks` (
-  `id_pks` varchar(15) NOT NULL,
+  `id_pks` int(15) NOT NULL,
   `nomor_pks` varchar(50) NOT NULL,
   `id_pimpinan_telkomsel` varchar(15) NOT NULL,
   `id_pic_telkomsel` varchar(15) NOT NULL,
-  `id_corporate` varchar(15) NOT NULL,
+  `id_corporate` int(15) NOT NULL,
   `id_paket` varchar(11) DEFAULT NULL,
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
-  `sign_pimpinan_corporate` varchar(50) NOT NULL,
-  `sign_pic_corporate` varchar(50) NOT NULL,
-  `sign_pimpinan_telkomsel` varchar(50) NOT NULL,
-  `sign_pic_telkomsel` varchar(50) NOT NULL
+  `sign_pimpinan_corporate` varchar(5) NOT NULL,
+  `sign_pic_corporate` varchar(5) NOT NULL,
+  `sign_pimpinan_telkomsel` varchar(5) NOT NULL,
+  `sign_pic_telkomsel` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -167,9 +168,7 @@ CREATE TABLE `tb_pks` (
 --
 
 INSERT INTO `tb_pks` (`id_pks`, `nomor_pks`, `id_pimpinan_telkomsel`, `id_pic_telkomsel`, `id_corporate`, `id_paket`, `start_date`, `end_date`, `sign_pimpinan_corporate`, `sign_pic_corporate`, `sign_pimpinan_telkomsel`, `sign_pic_telkomsel`) VALUES
-('00001', '010/LG.05/CM.73/VIII/2017', '67292', '88132', '1', 'P002', '2017-08-01', '2017-09-01', 'F', 'F', 'F', 'F'),
-('232323', 'efesfeds', '71829', '88145', '2', 'P004', '2017-08-01', '2017-08-31', 'F', 'F', 'F', 'F'),
-('3232', 'ererer', '71829', '87928', '3', 'P001', '2017-08-29', '2017-09-27', 'T', 'F', 'F', 'F');
+(2001, '010/LG.05/CM.73/VIII/2017', '67292', '87928', 103, 'P002', '2017-09-01', '2018-09-01', 'F', 'F', 'F', 'F');
 
 -- --------------------------------------------------------
 
@@ -190,8 +189,8 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`id_user`, `email`, `pass`, `nama`, `level`) VALUES
-(99, 'azizah@telkomsel.co.id', '1234', 'Azizah', 'Administrasi'),
-(106, 'aldi@telkomsel.co.id', '1234', 'aldiii', 'Administrasi'),
+(99, 'azizah@telkomsel.co.id', '1234', 'Azizarssdd,l,l,', 'Administrasi'),
+(106, 'aldi@telkomsel.co.id', '1234', 'paijo91115', 'Administrasi'),
 (108, 'imade_abimayu@telkomsel.co.id', '1234', 'I Made Abimayu', 'SPV');
 
 --
@@ -209,7 +208,7 @@ ALTER TABLE `tb_corporate`
 --
 ALTER TABLE `tb_list_nomor`
   ADD PRIMARY KEY (`id_list_msisdn`),
-  ADD KEY `FK_corporate2` (`id_corporate`);
+  ADD KEY `id_corporate` (`id_corporate`);
 
 --
 -- Indexes for table `tb_paket`
@@ -234,9 +233,8 @@ ALTER TABLE `tb_pimpinan_telkomsel`
 --
 ALTER TABLE `tb_pks`
   ADD PRIMARY KEY (`id_pks`),
-  ADD KEY `FK_corporate` (`id_corporate`),
-  ADD KEY `FK_pic_telkomsel` (`id_pic_telkomsel`),
-  ADD KEY `FK_pimpinan_telkomsel` (`id_pimpinan_telkomsel`),
+  ADD KEY `id_pimpinan_telkomsel` (`id_pimpinan_telkomsel`),
+  ADD KEY `id_corporate` (`id_corporate`),
   ADD KEY `id_paket` (`id_paket`);
 
 --
@@ -250,6 +248,21 @@ ALTER TABLE `tb_user`
 --
 
 --
+-- AUTO_INCREMENT for table `tb_corporate`
+--
+ALTER TABLE `tb_corporate`
+  MODIFY `id_corporate` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+--
+-- AUTO_INCREMENT for table `tb_list_nomor`
+--
+ALTER TABLE `tb_list_nomor`
+  MODIFY `id_list_msisdn` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10002;
+--
+-- AUTO_INCREMENT for table `tb_pks`
+--
+ALTER TABLE `tb_pks`
+  MODIFY `id_pks` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2002;
+--
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
@@ -262,15 +275,15 @@ ALTER TABLE `tb_user`
 -- Constraints for table `tb_list_nomor`
 --
 ALTER TABLE `tb_list_nomor`
-  ADD CONSTRAINT `FK_corporate2` FOREIGN KEY (`id_corporate`) REFERENCES `tb_corporate` (`id_corporate`) ON DELETE CASCADE;
+  ADD CONSTRAINT `FK_corporate_list` FOREIGN KEY (`id_corporate`) REFERENCES `tb_corporate` (`id_corporate`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tb_pks`
 --
 ALTER TABLE `tb_pks`
-  ADD CONSTRAINT `FK_corporate` FOREIGN KEY (`id_corporate`) REFERENCES `tb_corporate` (`id_corporate`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_corporate_pks` FOREIGN KEY (`id_corporate`) REFERENCES `tb_corporate` (`id_corporate`) ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_paket` FOREIGN KEY (`id_paket`) REFERENCES `tb_paket` (`id_paket`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_pic_telkomsel` FOREIGN KEY (`id_pic_telkomsel`) REFERENCES `tb_pic_telkomsel` (`id_pic_telkomsel`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_pic_telkomsel` FOREIGN KEY (`id_pimpinan_telkomsel`) REFERENCES `tb_pimpinan_telkomsel` (`id_pimpinan_telkomsel`) ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_pimpinan_telkomsel` FOREIGN KEY (`id_pimpinan_telkomsel`) REFERENCES `tb_pimpinan_telkomsel` (`id_pimpinan_telkomsel`) ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
