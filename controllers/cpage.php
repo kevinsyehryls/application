2d617250f6447b295667dd0729ed1acf59c97b52
kevinsyehryls <kevin.syehryls@gmail.com>
2017-09-04 18:09:23 +0800

    update design & other fitur

--- controllers/cpage.php
+++ controllers/cpage.php
@@ -94,6 +94,28 @@ class Cpage extends CI_Controller {
         );
         $this->load->view('wrapper', $data);
     }	
+
+    /* halaman Kelola vpks_ttd */
+    public function halpks_ttd(){
+        $data = array(
+            'apptitle' => 'E-PKS',
+            'appver'   => 'SISTEM INFORMASI PERJANJIAN KERJASAMA <Br> PT TELKOMSEL BALI <i>DENGAN PELANGGAN CORPORATE</i> <br> BERBASIS BOOTSRAP',
+            'title'    => 'Pending Tanda Tangan',
+            'page'     => 'pages/vpks_ttd'
+        );
+        $this->load->view('wrapper', $data);
+    }
+
+    /* halaman Kelola vpks_end */
+    public function halpks_end(){
+        $data = array(
+            'apptitle' => 'E-PKS',
+            'appver'   => 'SISTEM INFORMASI PERJANJIAN KERJASAMA <Br> PT TELKOMSEL BALI <i>DENGAN PELANGGAN CORPORATE</i> <br> BERBASIS BOOTSRAP',
+            'title'    => 'PKS Akan Berakhir',
+            'page'     => 'pages/vpks_end'
+        );
+        $this->load->view('wrapper', $data);
+    }
 	
 	/* halaman Report */
     public function halrpt(){
