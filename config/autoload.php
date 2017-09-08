ed3ccca3f5a4925c943003a5fc9e6798a1c18678
Pirman Duiana <pirmandwiana@gmail.com>
2017-09-09 00:52:44 +0800

    autoload file tambah file

--- config/autoload.php
+++ config/autoload.php
@@ -88,7 +88,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url','text','form','captcha');
+$autoload['helper'] = array('url','text','form','captcha', 'html', 'file');
 
 /*
 | -------------------------------------------------------------------
