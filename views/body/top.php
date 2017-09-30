687f2b0f4b74d12a3327b625e0ab8478c3875d28
kevinsyehryls <kevin.syehryls@gmail.com>
2017-10-01 05:09:20 +0800

    update Design & import list nomor

--- views/body/top.php
+++ views/body/top.php
@@ -105,7 +105,7 @@
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                  <a href="<?php echo base_url(); ?>index.php/cpage/halprof" class="btn btn-default btn-flat">Profile</a>
                 </div>
                 <div class="pull-right">
                   <a href="#" class="btn btn-default btn-flat">Sign out</a>
