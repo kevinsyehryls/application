ccd2a55803019cc24b462657ee426e8966fca9f4
kevinsyehryls <kevin.syehryls@gmail.com>
2017-10-01 05:05:34 +0800

    update Design & import list nomor

--- controllers/ccrudindx.php
+++ controllers/ccrudindx.php
@@ -17,22 +17,6 @@ class Ccrudindx extends CI_Controller {
 				<div class="row">
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>150</h3>
-              <p><strong>TTD TERTUNDA</strong></p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-alert-circled"></i>
-            </div>
-            <a href="<?php echo base_url(); ?>index.php/cpage/halpks_ttd" class="small-box-footer">Info Lebih Lanjut <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <?php
-        }
-        ?>
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
           <div class="small-box bg-red">
             <div class="inner">
               <h3><?php echo $row->jml; ?></h3>
@@ -44,7 +28,27 @@ class Ccrudindx extends CI_Controller {
              <a href="<?php echo base_url(); ?>index.php/cpage/halpks_end" class="small-box-footer">Info Lebih Lanjut <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
+        <?php
+        }
+         $this->load->model('mcrudpks');
+         $query = $this->mcrudpks->jumlah_upload();
+         foreach($query->result() as $row){
+        ?>
+       <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3><?php echo $row->jml1; ?></h3>
+              <p><strong>PKS BELUM TERUPLOAD</strong></p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-alert-circled"></i>
+            </div>
+            <a href="<?php echo base_url(); ?>index.php/cpage/halpks_ttd" class="small-box-footer">Info Lebih Lanjut <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
     <?php
     }
+  }
 }
 ?>
