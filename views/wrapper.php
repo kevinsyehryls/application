d6bac1f3728a0d63608111de420f5e90c42403ba
kevinsyehryls <kevin.syehryls@gmail.com>
2017-10-01 15:59:57 +0800

    update penambahan data eksport & merapikan design

--- views/wrapper.php
+++ views/wrapper.php
@@ -2,6 +2,6 @@
 	require_once('body/top.php');	
 	require_once('body/left.php');
 	require_once('body/cont.php');
-	require_once('body/side.php');
 	require_once('body/foot.php');
+	require_once('body/side.php');
 ?>
