90250b9815a455427a900cd927f591551a9144e4
kevinsyehryls <kevin.syehryls@gmail.com>
2017-10-01 23:51:27 +0800

    merapikan design dan finishing web

--- views/body/menu/menu_adm.php
+++ views/body/menu/menu_adm.php
@@ -3,57 +3,50 @@
             <i class="fa fa-list"></i> <span>BERANDA</span>
           </a>
         </li>
+        <li class="header"><strong>KELOLA PELANGGAN CORPORATE</strong></li>
         <li>
-          <a href="<?php echo base_url(); ?>index.php/cpage/haluser">
-            <i class="fa  fa-cogs"></i> <span>KELOLA USER</span>
+          <a href="<?php echo base_url(); ?>index.php/cpage/halcorp">
+            <i class="fa  fa-users"></i> <span>Kelola Corporate</span>
           </a>
         </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-user-plus"></i> <span> KELOLA PEJABAT</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+        <li>
+          <a href="<?php echo base_url(); ?>index.php/cpage/hallist">
+            <i class="fa  fa-users"></i> <span>Kelola List Nomor</span>
           </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo base_url(); ?>index.php/cpage/halppn"><i class="fa fa-cog"></i> Kelola Pimpinan Telkomsel</a></li>
-            <li><a href="<?php echo base_url(); ?>index.php/cpage/halstaf"><i class="fa fa-cog"></i> Kelola PIC Telkomsel</a></li>
-          </ul>
         </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-edit"></i> <span>PELANGGAN CORPORATE</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+        <li class="header"><strong>KELOLA PKS CORPORATE</strong></li>
+        <li>
+          <a href="<?php echo base_url(); ?>index.php/cpage/halpks">
+            <i class="fa  fa-file-powerpoint-o"></i> <span>Kelola PKS</span>
           </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo base_url(); ?>index.php/cpage/halcorp"><i class="fa fa-group"></i> Corporate</a></li>
-            <li><a href="<?php echo base_url(); ?>index.php/cpage/hallist"><i class="fa fa-file-text-o"></i> List Nomor Corporate</a></li>
-          </ul>
         </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-edit"></i> <span>PKS CORPORATE</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+        <li>
+          <a href="<?php echo base_url(); ?>index.php/cpage/halpks_ttd">
+            <i class="fa  fa-file-powerpoint-o"></i> <span>Pending Tanda Tangan</span>
           </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo base_url(); ?>index.php/cpage/halpks"><i class="fa  fa-file-o"></i> PKS</a></li>
-            <li><a href="<?php echo base_url(); ?>index.php/cpage/halpks_ttd"><i class="fa fa-minus-square"></i> Pending Tanda Tangan</a></li>
-            <li><a href="<?php echo base_url(); ?>index.php/cpage/halpks_end"><i class="fa fa-sign-out"></i> PKS Akan Berakhir</a></li>
-          </ul>
         </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-files-o"></i> <span>REPORT</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+        <li>
+          <a href="<?php echo base_url(); ?>index.php/cpage/halpks_end">
+            <i class="fa  fa-file-powerpoint-o"></i> <span>PKS Akan Berakhir</span>
           </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo base_url(); ?>index.php/cpage/halrpt"><i class="fa fa-arrow-circle-down"></i> Eksport Data</a></li>
-            <li><a href="<?php echo base_url(); ?>index.php/cpage/halgraf"><i class="fa fa-line-chart"></i> Lihat Grafik</a></li>
-          </ul>
         </li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
+        <li class="header"></li>
