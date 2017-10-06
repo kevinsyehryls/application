b8506878e5a1ed36e4eb27705a4e5c333fa5578c
kevinsyehryls <kevin.syehryls@gmail.com>
2017-10-06 23:08:54 +0700

    merapikan web

--- views/pages/vgraf.php
+++ views/pages/vgraf.php
@@ -1,24 +1,22 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <h1>
-      <?php echo $title ?>
-      <?php echo $apptitle?>
-    </h1>
-    <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-      <li><a href="#">Examples</a></li>
-      <li class="active">Blank page</li>
-    </ol>
-  </section>
+   <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        <?php echo $title ?>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-files-o"></i> MENU </a></li>
+        <li class="active"><?php echo $title ?></li>
+      </ol>
+    </section>
 
   <!-- Main content -->
   <section class="content">
     <!-- Default box -->
     <div class="box">
       <div class="box-header with-border">
-        <h3 class="box-title">PKS Graph</h3>
+        <h3 class="box-title">Grafik Bulanan Pembuatan PKS</h3>
 
         <div class="box-tools pull-right">
           <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
